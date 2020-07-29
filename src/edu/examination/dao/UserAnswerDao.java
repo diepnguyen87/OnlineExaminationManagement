32281483cd5d75a7e6ea80b1a2fd5a03654187db
Diep Nguyen <diepniit.nguyenhong@gmail.com>
2020-07-29 17:02:31 +0700

    add new code

---
+++ src/edu/examination/dao/UserAnswerDao.java
@@ -0,0 +1,8 @@
+package edu.examination.dao;
+
+import edu.examination.entity.UserAnswerEntity;
+
+public interface UserAnswerDao {
+	
+	public int addUserAnswer(UserAnswerEntity userAnswer);
+}
---
+++ src/edu/examination/dao/impl/UserAnswerDaoImpl.java
@@ -0,0 +1,61 @@
+package edu.examination.dao.impl;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import edu.examination.config.ConnectionFactory;
+import edu.examination.dao.UserAnswerDao;
+import edu.examination.entity.UserAnswerEntity;
+
+public class UserAnswerDaoImpl implements UserAnswerDao{
+
+	private Connection connection = null;
+	private PreparedStatement ptmt = null;
+	private ResultSet resultSet = null;
+	
+	public UserAnswerDaoImpl() {
+		
+	}
+	
+	private Connection getConnection(){
+		Connection conn;
+		conn = ConnectionFactory.getInstance().getConnection();
+		return conn;
+	}
+	
+	@Override
+	public int addUserAnswer(UserAnswerEntity userAnswer) {
+		int addedRows=0;
+		try {
+		String queryString = "Insert into user_exam_question_answer(user_id, exam_id, question_id, user_answer_option) "
+							+ "values(?,?,?,?)";
+			connection = getConnection();
+			ptmt = connection.prepareStatement(queryString);
+			ptmt.setString(1, userAnswer.getUserID());
+			ptmt.setString(2, userAnswer.getExamID());
+			ptmt.setString(3, userAnswer.getQuestionID());
+			ptmt.setString(4, userAnswer.getUserOptionID());
+			addedRows = ptmt.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try{
+				if(ptmt != null){
+					ptmt.close();
+				}
+				if(connection != null){
+					connection.close();
+				}
+			}catch(SQLException e){
+				e.printStackTrace();
+			}catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return addedRows;
+	}
+}
